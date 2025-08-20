95ea1b5c30556b9cf3fde610bf46b3832ed349d5
ayyucedemirbas <a.ayyuced@gmail.com>
2025-08-20 16:16:18 +0300

    longest common subsequence

---
+++ Basics/longest_common_subsequence_dna.py
@@ -0,0 +1,24 @@
+seq1 = "ATGCATGC"
+seq2 = "TGCATGCA"
+m, n = len(seq1), len(seq2)
+dp = [[0] * (n + 1) for _ in range(m + 1)]
+for i in range(1, m + 1):
+    for j in range(1, n + 1):
+        if seq1[i-1] == seq2[j-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+lcs = []
+i, j = m, n
+while i > 0 and j > 0:
+    if seq1[i-1] == seq2[j-1]:
+        lcs.append(seq1[i-1])
+        i -= 1
+        j -= 1
+    elif dp[i-1][j] > dp[i][j-1]:
+        i -= 1
+    else:
+        j -= 1
+
+lcs.reverse()
+print(''.join(lcs))
